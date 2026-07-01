2e7beb0f8c85065966a628788631f467a7e0f03c
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-01 21:46:22 +0200

    feat: implement async init_db function with metadata discovery

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.core.config import DATABASE_URL
+from app.models.startup import Startup
+from sqlmodel import SQLModel
 
 engine = create_async_engine(url=DATABASE_URL, echo=True)
 
@@ -8,3 +10,8 @@ async_maker_factory = async_sessionmaker(
     expire_on_commit=False,
     class_=AsyncSession
 )
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
