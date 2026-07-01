0034b34622d0584b4a0571a7736c001a72a70b7d
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-01 21:16:11 +0200

    feat: successfully implement database configuration and session factory

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,9 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.core.config import DATABASE_URL
 
 engine = create_async_engine(url=DATABASE_URL, echo=True)
 
-async_session_factory = async_sessionmaker(
+async_maker_factory = async_sessionmaker(
     bind=engine,
     expire_on_commit=False,
     class_=AsyncSession
