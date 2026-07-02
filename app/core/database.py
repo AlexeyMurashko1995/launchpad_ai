bf477639f4246560f195611c60bd96dca5e486e3
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-02 21:23:58 +0200

    feat: implement ACID-compliant startup creation function

--- app/core/database.py
+++ app/core/database.py
@@ -14,3 +14,10 @@ async_maker_factory = async_sessionmaker(
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def create_startup(name: str, category: str):
+    async with async_maker_factory() as session:
+        async with session.begin():
+            startup = Startup(name=name, category=category)
+            session.add(startup)
