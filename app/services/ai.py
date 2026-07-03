899ba97afeb802670ffcbbb87c1946793533e7a7
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-03 21:05:00 +0200

    feat: update generate_mock_analysis to use dynamic startup data

--- app/services/ai.py
+++ app/services/ai.py
@@ -1,6 +1,8 @@
-from app.core.database import update_startup_ai_response
+from app.core.database import update_startup_ai_response, get_startup_by_id
 
 
 async def generate_mock_analysis(startup_id: int):
-    mock_text = 'string'
-    await update_startup_ai_response(startup_id, mock_text)
+    new_startup = await get_startup_by_id(startup_id)
+    if new_startup:
+        mock_text = f'Startup name: {new_startup.name}; category: {new_startup.category}'
+        await update_startup_ai_response(startup_id, mock_text)
