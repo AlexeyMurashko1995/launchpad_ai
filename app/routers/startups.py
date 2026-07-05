79dd8a8e31990bd11a56e6f7c805ce54ea6158ae
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-05 21:56:49 +0200

    feat: implement POST endpoint for creating startups in router

--- app/routers/startups.py
+++ app/routers/startups.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from app.core.database import get_all_startups
+from app.core.database import create_startup
 
 router = APIRouter(prefix='/startups', tags=['Startups'])
 
 @router.get('/')
 async def get_startups():
     return await get_all_startups()
+
+
+@router.post('/')
+async def add_new_startup(name: str, category: str, background_tasks: BackgroundTasks):
+    return await create_startup(name=name, category=category, background_tasks=background_tasks)
