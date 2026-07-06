59ca3735bdc75752e7d573ec2b94a0dfbb7e3ceb
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-06 21:37:32 +0200

    feat: add GET endpoint to fetch a single startup by ID

--- app/routers/startups.py
+++ app/routers/startups.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, BackgroundTasks
 from app.core.database import get_all_startups
 from app.core.database import create_startup
 from app.models.startup import StartupCreate
+from app.core.database import get_startup_by_id
 
 router = APIRouter(prefix='/startups', tags=['Startups'])
 
@@ -13,3 +14,8 @@ async def get_startups():
 @router.post('/')
 async def add_new_startup(startup_data: StartupCreate, background_tasks: BackgroundTasks):
     return await create_startup(name=startup_data.name, category=startup_data.category, background_tasks=background_tasks)
+
+
+@router.get('/{startup_id}')
+async def get_target_startup(startup_id: int):
+    return await get_startup_by_id(startup_id)
