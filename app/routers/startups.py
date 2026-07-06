b82282bc7afe67a20e7116e54818926ea22fe8c1
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-06 22:28:56 +0200

    feat: implement DELETE endpoint for startups by ID

--- app/routers/startups.py
+++ app/routers/startups.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, BackgroundTasks, HTTPException, status
 from app.core.database import get_all_startups
 from app.core.database import create_startup
 from app.models.startup import StartupCreate
+from app.core.database import delete_startup
 from app.core.database import get_startup_by_id
 from app.models.startup import StartupPublic
 
@@ -23,3 +24,11 @@ async def get_target_startup(startup_id: int):
     if not target_startup:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail='Startup not found')
     return target_startup
+
+
+@router.delete('/{startup_id}')
+async def remove_startup(startup_id: int):
+    deleted_startup = await delete_startup(startup_id)
+    if not deleted_startup:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='Startup not found')
+    return {'message': f'startup #{startup_id} was successfully deleted'}
