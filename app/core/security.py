3739353e6a53af208dbd2c454857b6f6ab316b46
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-11 21:42:07 +0200

    feat: implement JWT decode logic and error handling in get_current_user

--- app/core/security.py
+++ app/core/security.py
@@ -1,9 +1,21 @@
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_db
+import jwt
+from jwt.exceptions import PyJWTError
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
+SECRET_KEY = 'my_secret_key'
+ALGORITHM = 'HS256'
 
 async def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            raise HTTPException(status_code=401, detail='Could not validate credentials')
+        return username
+    except PyJWTError:
+        raise HTTPException(status_code=401, detail='Could not validate credentials')
