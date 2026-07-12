e370dd88871b3176ddef35a1fbd356df45330222
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-12 21:18:06 +0200

    feat: add UserCreate and UserPublic schemas to user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,17 @@
 from sqlmodel import SQLModel, Field
+from pydantic import BaseModel
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     username: str = Field(unique=True)
     hashed_password: str
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
