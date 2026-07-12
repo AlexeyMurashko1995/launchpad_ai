48ba8876f99b37ffe7058ec3e463b93640eb4297
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-12 21:29:04 +0200

    feat: register auth router in main application instance

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routers.startups import router as startup_router
+from app.routers.auth import router as auth_router
 
 app = FastAPI()
 
 app.include_router(startup_router)
+app.include_router(auth_router)
