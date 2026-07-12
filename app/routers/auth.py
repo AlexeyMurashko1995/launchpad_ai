f6306030ff75b636699259bfb3cdb049ce306e0a
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-12 21:47:23 +0200

    feat: add username uniqueness check to register_user endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.models.user import User, UserCreate
 from app.core.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
 
 
 @router.post('/register')
 async def register_user(user_data: UserCreate, session: AsyncSession = Depends(get_db)):
-    return {'status': 'ok'}
+    query = select(User).where(user_data.username == User.username)
+    result = await session.execute(query)
+    existing_user = result.scalar_one_or_none()
+    if existing_user:
+        raise HTTPException(status_code=400, detail='Username already taken')
+
