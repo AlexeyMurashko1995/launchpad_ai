ec0e3a711895e45d56e271ea55ed9a25aa9b4f7f
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-13 21:46:57 +0200

    feat: implement user login flow and automatic DB tables creation on startup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from app.routers.startups import router as startup_router
 from app.routers.auth import router as auth_router
+from app.core.database import init_db
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(startup_router)
 app.include_router(auth_router)
